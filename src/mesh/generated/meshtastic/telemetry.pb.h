bd5815b0edbbd0b1f1892b57f30b0fb09ec30338
Leon Anavi <leon@anavi.org>
2025-12-24 12:53:29 +0200

    telemetry.pb.h: Add HTU21D-F sensor
    Add HTU21D-F humidity and temperature sensor as 46.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- src/mesh/generated/meshtastic/telemetry.pb.h
+++ src/mesh/generated/meshtastic/telemetry.pb.h
@@ -103,7 +103,9 @@ typedef enum _meshtastic_TelemetrySensorType {
     /* TSL2561 light sensor */
     meshtastic_TelemetrySensorType_TSL2561 = 44,
     /* BH1750 light sensor */
-    meshtastic_TelemetrySensorType_BH1750 = 45
+    meshtastic_TelemetrySensorType_BH1750 = 45,
+    /* HTU21D-F humidity and temperature sensor */
+    meshtastic_TelemetrySensorType_HTU21D = 46
 } meshtastic_TelemetrySensorType;
 
 /* Struct definitions */
